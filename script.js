a19decc06a8c6f4c476110356ad0bcd9e516c456
avinashhhchavan-cloud <avinashhhchavan@gmail.com>
2026-04-06 15:24:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-// Smooth scroll
 function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
-
-// Dark/Light Mode
-function toggleTheme() {
-  document.body.classList.toggle("light");
-}
-
-// Modal
-function openModal(src) {
-  document.getElementById("modal").style.display = "flex";
-  document.getElementById("modalImg").src = src;
-}
-
-function closeModal() {
-  document.getElementById("modal").style.display = "none";
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
 }
